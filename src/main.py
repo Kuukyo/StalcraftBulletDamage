f117d091c0c7f17a8e1d2f1fd26ad76f3ed0f5be
Kuukyo <kvvkyo@gmail.com>
2024-03-10 00:38:15 +0100

    Added 2nd graph for comparison purposes

--- src/main.py
+++ src/main.py
@@ -4,24 +4,33 @@ import ttk
 import numpy as np
 
 
-guns = ["ASh-12", "АК-15"]
+guns = ["SA-58 CTC", "АК-15"]
 bullet_res = 351.23
+bullet_res2 = 392.83
 vitality = 115
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+fig.suptitle('TTK of Guns +15 using AP ammo')
 
 for name in guns:
     weapon = gun.Gun(name)
     ttkc = ttk.TTKCalculator(weapon, vitality, bullet_res, True, "ceramic")
+    ttkc2 = ttk.TTKCalculator(weapon, vitality, bullet_res2, True, "ceramic")
     x = np.arange(150)
     y = []
+    y2 = []
 
     for d in x:
         y.append(ttkc.ttk(d))
+        y2.append(ttkc2.ttk(d))
 
-    plt.plot(x, y, label=name)
+    ax1.plot(x, y, label=name)
+    ax2.plot(x, y2, label=name)
 
-plt.xlabel("Distance In Meters")
-plt.ylabel("Time To Kill In Seconds")
-plt.title(f"TTK Of Guns +15 against {vitality} Vitality and {bullet_res} Bullet Resistance using AP Ammo")
+ax1.set(xlabel="Distance In Meters", ylabel="Time To Kill In Seconds", title=f"Against {vitality} Vitality and {bullet_res} Bullet Resistance using AP Ammo")
+ax2.set(xlabel="Distance In Meters", ylabel="Time To Kill In Seconds", title=f"Against {vitality} Vitality and {bullet_res2} Bullet Resistance using AP Ammo")
 
 plt.legend()
 plt.show()
+
+
